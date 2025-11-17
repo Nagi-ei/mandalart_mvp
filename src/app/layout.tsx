f3732a39fe0f556297396db4ce82de75ab2fb19a
Nagi-ei <nakt4453@gmail.com>
2025-11-18 06:51:48 +0900

    🎉 Setup project
    install dependencies, add basic settings and docs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+export const pretendard = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-pretendard",
 });
 
 export const metadata: Metadata = {
@@ -23,12 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`dark ${pretendard.className}`}>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
